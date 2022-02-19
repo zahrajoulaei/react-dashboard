36f8c71c411439c0665aee9420b4acdbb0c8a695
ZahraJou <zahrajoulaei@gmail.com>
2022-02-19 18:52:34 +0100

    :+1: add active tab 1 and 2

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -5,12 +5,17 @@ import SecondTab from '../all-tabs/SecondTab';
 import styles from '../all-tabs/tabs.module.css'
 export default function Tab() {
     const [activeTab, setActiveTab] = useState("tab1");
+    const handleTab1 = () => {
+        setActiveTab("tab1");
+      };
+      const handleTab2 = () => {
+        setActiveTab("tab2");
+      };
     return (
         <div className={styles.Tabs}>
-            {/* Tab nav */}
             <ul className={styles.nav}>
-                <li className={activeTab === "tab1" ? "active" : ""}>Tab 1</li>
-                <li className={activeTab === "tab2" ? "active" : ""}>Tab 2</li>
+                <li   onClick={handleTab1} className={activeTab === "tab1" ? "active" : ""}>Tab 1</li>
+                <li   onClick={handleTab2} className={activeTab === "tab2" ? "active" : ""}>Tab 2</li>
             </ul>
             <div className={styles.outlet}>
             {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
